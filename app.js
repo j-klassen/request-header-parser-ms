a7fbf54720a845d36e3c1016703fb0007eb4d128
Jake Klassen <jklassendev@gmail.com>
2016-02-05 17:04:18 +0000

    extract ipv4 address

--- app.js
+++ app.js
@@ -4,13 +4,15 @@
 
 const express = require('express');
 const app = express();
+// Handles IPv4, IPv6 addresses
+const ipaddr = require('ipaddr.js');
 const port = process.env.PORT || 3000;
 
 const apiRouter = express.Router();
 
 apiRouter.get('/whoami', (req, res) => {
 	res.json({
-		ip: req.ip,
+		ip: ipaddr.process(req.ip).toString(),
 		language: req.acceptsLanguages()[0] || 'unknown',
 		software: req.headers['user-agent'].match(/\((.*?)\)/)[0] || 'unknown'
 	});
