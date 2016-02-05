19c7158ef21b50d0d8b77e2e142dc1e377cdaf39
Jake Klassen <jklassendev@gmail.com>
2016-02-05 14:56:11 +0000

    initial solution

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const apiRouter = express.Router();
 
+apiRouter.get('/whoami', (req, res) => {
+	res.json({
+		ip: req.ip,
+		language: req.acceptsLanguages()[0] || 'unknown',
+		software: req.headers['user-agent'].match(/\((.*?)\)/)[0] || 'unknown'
+	});
+});
+
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
